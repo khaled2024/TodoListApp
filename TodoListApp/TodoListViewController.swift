14179d9e0a21fef255b6eb20409da5ea56862329
khaled hussien <57367756+khaled2024@users.noreply.github.com>
2021-12-07 19:24:34 +0200

    create and implement the local tableView with hard coded array

--- TodoListApp/TodoListViewController.swift
+++ TodoListApp/TodoListViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-
+    let itemArray = ["khaled","hussien","khalifa"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
 
 }
 

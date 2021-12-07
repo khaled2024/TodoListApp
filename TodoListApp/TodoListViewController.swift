14428d521cc29a9ed61efebd53724f5aa7d1795a
khaled hussien <57367756+khaled2024@users.noreply.github.com>
2021-12-07 18:55:18 +0200

    initial commit

--- TodoListApp/TodoListViewController.swift
+++ TodoListApp/TodoListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TodoListViewController.swift
 //  TodoListApp
 //
 //  Created by KhaleD HuSsien on 07/12/2021.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TodoListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()

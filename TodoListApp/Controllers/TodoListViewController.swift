decb737266216e9a4ba4c18b73277458791eaa70
khaled hussien <57367756+khaled2024@users.noreply.github.com>
2021-12-08 15:39:06 +0200

    implement coreData and know about (Create,Read,Update,Remove)😁 loadData and SaveData and searchBar Delegate and dismiss the keyboard (resignFirstResponder)👻

--- TodoListApp/Controllers/TodoListViewController.swift
+++ TodoListApp/Controllers/TodoListViewController.swift
@@ -6,47 +6,48 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
     
     //MARK: - variables
     var itemArray = [Item]()
-    
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Item.plist")
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     //MARK: - lifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        print( FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        
         loadData()
     }
     //MARK: - functions
     private func saveItems(){
-        let encoder = PropertyListEncoder()
           do{
-              let data = try encoder.encode(itemArray)
-              try data.write(to: dataFilePath!)
+              try context.save()
           }catch{
-              print("Error encoding itemArray!\(error)")
+             print("Error saving context \(error)")
           }
         self.tableView.reloadData()
     }
-    private func loadData(){
-        if let data = try? Data(contentsOf: dataFilePath!){
-            let decoder = PropertyListDecoder()
-            do {
-                itemArray = try decoder.decode([Item].self, from: data)
-            }catch{
-                print("Erroe decoding item arrar \(error)")
-            }
+    private func loadData(with request: NSFetchRequest<Item> = Item.fetchRequest()){
+        do{
+           itemArray = try context.fetch(request)
         }
+        catch{
+            print("Error for feching Item \(error)")
+        }
+        self.tableView.reloadData()
     }
     //MARK: - Actions
     @IBAction func addBtnTapped(_ sender: UIBarButtonItem) {
         var textField = UITextField()
         let alert = UIAlertController(title: "Add new ToDo item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add item", style: .default) { action in
-            let newItem = Item()
+           
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
+            newItem.done = false
             self.itemArray.append(newItem)
             self.saveItems()
         }
@@ -58,7 +59,7 @@ class TodoListViewController: UITableViewController {
         self.present(alert, animated: true, completion: nil)
     }
 }
-//MARK: - UiTableViewDelegate
+//MARK: - Extension UiTableViewDelegate
 extension TodoListViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -72,9 +73,32 @@ extension TodoListViewController {
         return cell
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+        
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         saveItems()
         // here to didnt keeping selecting the cell after we select it
         tableView.deselectRow(at: indexPath, animated: true)
     }
+}
+//MARK: - UISearchBarDelegate
+extension TodoListViewController: UISearchBarDelegate{
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        loadData(with: request)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadData()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
+
 }

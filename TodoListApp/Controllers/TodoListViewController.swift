9c7d4491f9de9de7b834c01e59781c5186d9cf91
khaled hussien <57367756+khaled2024@users.noreply.github.com>
2021-12-08 12:14:47 +0200

    insted of using userDefault we used coding and decoding dataFilePath saveItems loadData

--- TodoListApp/Controllers/TodoListViewController.swift
+++ TodoListApp/Controllers/TodoListViewController.swift
@@ -10,27 +10,45 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     //MARK: - variables
-    var itemArray = ["khaled","hussien","khalifa"]
-    let Defaults = UserDefaults.standard
+    var itemArray = [Item]()
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Item.plist")
     
     //MARK: - lifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let items = Defaults.array(forKey: "ToDoItemArr") as? [String]{
-            itemArray = items
-        }
-        
+        loadData()
     }
-    
+    //MARK: - functions
+    private func saveItems(){
+        let encoder = PropertyListEncoder()
+          do{
+              let data = try encoder.encode(itemArray)
+              try data.write(to: dataFilePath!)
+          }catch{
+              print("Error encoding itemArray!\(error)")
+          }
+        self.tableView.reloadData()
+    }
+    private func loadData(){
+        if let data = try? Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            }catch{
+                print("Erroe decoding item arrar \(error)")
+            }
+        }
+    }
     //MARK: - Actions
-    
     @IBAction func addBtnTapped(_ sender: UIBarButtonItem) {
         var textField = UITextField()
         let alert = UIAlertController(title: "Add new ToDo item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add item", style: .default) { action in
-            self.itemArray.append(textField.text!)
-            self.Defaults.set(self.itemArray, forKey: "ToDoItemArr")
-            self.tableView.reloadData()
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+            self.saveItems()
         }
         alert.addTextField { alertTextField in
             alertTextField.placeholder = "Create new item"
@@ -39,29 +57,23 @@ class TodoListViewController: UITableViewController {
         alert.addAction(action)
         self.present(alert, animated: true, completion: nil)
     }
-    
 }
 //MARK: - UiTableViewDelegate
 extension TodoListViewController {
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
-
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        // value = condition ? ValueIfTrue : ValueIfFalse
+        cell.accessoryType = item.done ? .checkmark : .none
         return cell
     }
-    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        saveItems()
         // here to didnt keeping selecting the cell after we select it
         tableView.deselectRow(at: indexPath, animated: true)
     }
---
+++ TodoListApp/Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  TodoListApp
+//
+//  Created by KhaleD HuSsien on 08/12/2021.
+//
+
+import Foundation
+
+class Item: Codable{
+    var title: String = ""
+    var done: Bool = false
+}
